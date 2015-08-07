5b4bd58c64424da2559d8fd02f55f104901df6dd
mj1618 <matthew.stephen.james@gmail.com>
2015-08-08 03:33:39 +0800

    Errors put in, not tested fully

--- unit.py
+++ unit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from point import *
-
+import copy
 class Command:
     pass
 
@@ -16,10 +16,12 @@ class Unit:
     def __init__(self, pts, pivot):
         self.pivot = pivot
         self.mask = None
-        self.moves=[]
         self.mask = [pt - pivot for pt in pts]
+        self.old_states = []
+        self.current_rotation=NE
 
     def command(self,cmd):
+        self.old_states.append([copy.deepcopy(self.pivot),self.current_rotation])
         if type(cmd) is Move:
             self.move(cmd.dir)
         else:
@@ -27,18 +29,32 @@ class Unit:
 
     def move(self, dir):
         self.pivot += dir
-        self.moves.append(Move(dir))
 
     def rotate(self, dir):
         self.mask = [pt.rotate(dir) for pt in self.mask]
-        self.moves.append(Rotation(dir))
+
 
     def undo(self,last_command):
         if type(last_command) is Move:
             self.pivot -= last_command.dir
         else:
             self.mask = [pt.rotate(not last_command.dir) for pt in self.mask]
+        self.old_states.pop()
 
+    def is_error(self):
+        for state in self.old_states:
+            if self.pivot == state[0] and self.rotation_matches(state[1]):
+                return True
+        return False
+
+    def rotation_matches(self,rotation):
+        unit = copy.deepcopy(self)
+        while unit.current_rotation != rotation:
+            unit.rotate(clockwise)
+        for i in range(0,len(unit.mask)):
+            if(unit.mask[i] != self.mask[i]):
+                return False
+        return True
 
     def get_pts(self):
         pts = []
