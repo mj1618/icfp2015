4ed4899a26677210e92fc2da6073c74499df0a97
sqweek <sqweek@gmail.com>
2015-08-08 00:42:47 +0800

    Refactor unit class.

--- unit.py
+++ unit.py
@@ -1,69 +1,33 @@
 #!/usr/bin/env python
-import copy
 from point import *
+
+class Command:
+    pass
+
+class Move(Command):
+    def __init__(self, dir):
+        self.dir = dir
+
+class Rotation(Command):
+    def __init__(self, clockwise):
+        self.clockwise = clockwise
 
 class Unit:
     def __init__(self, pts, pivot):
         self.pivot = pivot
         self.pts = pts
-        self.previous = None
         self.mask = None
         self.moves=[]
         for pt in pts:
             self.mask.append([pt.__sub__(pivot)])
 
-    def pre_move(self):
-        self.previous = copy.deepcopy(self)
+    def move(self,dir):
+        self.pts = [p + dir for p in self.pts]
+        self.moves.append(Move(dir))
 
-    def move_w(self):
-        self.pre_move()
-        for pt in self.pts:
-            pt += W
-
-    def move_e(self):
-        self.pre_move()
-        for pt in self.pts:
-            pt += E
-    def move_sw(self):
-        self.pre_move()
-        for pt in self.pts:
-            pt += SW
-
-    def move_se(self):
-        self.pre_move()
-        for pt in self.pts:
-            pt += SE
-    def move_nw(self):
-        self.pre_move()
-        for pt in self.pts:
-            pt += NW
-
-    def move_ne(self):
-        self.pre_move()
-        for pt in self.pts:
-            pt += NE
-
-    def move(self,cmd):
-        if cmd=='E':
-            self.move_e()
-        elif cmd=='W':
-            self.move_w()
-        if cmd=='SE':
-            self.move_se()
-        elif cmd=='SW':
-            self.move_sw()
-        self.moves.append(cmd)
-
-    def move_opposite(self,cmd):
-        if cmd=='E':
-            self.move_w()
-        elif cmd=='W':
-            self.move_e()
-        if cmd=='SE':
-            self.move_nw()
-        elif cmd=='SW':
-            self.move_ne()
-        self.moves.append(cmd)
+    def undo_last_move(self):
+        last_move = self.moves.pop()
+        self.pts = [p - move.dir for p in self.pts]
 
     def is_filled(self,x,y):
         for pt in self.pts:
