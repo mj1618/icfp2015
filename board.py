619c50f529677d7ddf39d0eb8768abbc3176cf7a
mj1618 <matthew.stephen.james@gmail.com>
2015-08-08 00:25:21 +0800

    board and unit updates

--- board.py
+++ board.py
@@ -7,20 +7,41 @@ RNG_INC = 12345
 RNG_MASK = 0x7fff0000
 RNG_TRUNC = 16
 
+RENDER_TOP_LEFT = u"╔═══"
+RENDER_TOP_MID = u"╦═══"
+RENDER_TOP_RIGHT = u"╗\n"
+RENDER_BODY_LEFTMID = u"║ {} "
+RENDER_BODY_RIGHT = u"║\n"
+RENDER_BODY_OFFSET = u"  "
+RENDER_ODDJOIN_LEFT = u"╚═╦═"
+RENDER_ODDJOIN_MID = u"╩═╦═"
+RENDER_ODDJOIN_RIGHT = u"╩═╗\n"
+RENDER_EVENJOIN_LEFT = u"╔═╩═"
+RENDER_EVENJOIN_MID = u"╦═╩═"
+RENDER_EVENJOIN_RIGHT = u"╦═╝\n"
+RENDER_BOTTOM_LEFT = u"╚═══"
+RENDER_BOTTOM_MID = u"╩═══"
+RENDER_BOTTOM_RIGHT = u"╝\n"
+
+RENDER_GRID_FILL = u"#"
+RENDER_GRID_EMPTY = u" "
+
+
 class Board:
 
     solutions = None
 
     def __init__(self, width, height, fills, units, seed):
-        self.board = [[0 for x in range(width)] for x in range(height)]
+        self.grid = [[0 for x in range(width)] for x in range(height)]
         self.width = width
         self.height = height
         self.units = units
         self.seed = seed
         self.old_seed = None
+        self.current_unit = None
 
         for fill in fills:
-            self.board[fill.x][fill.y] = 1
+            self.grid[fill.x][fill.y] = 1
 
         self.next_unit()
 
@@ -36,14 +57,51 @@ class Board:
 
     def is_incorrect(self):
         for pt in self.current_unit.pts:
-            if pt.x>self.width or pt.y>self.height or pt.x<0 or pt.y<0 or self.board[pt.x][pt.y]==1:
+            if pt.x>self.width or pt.y>self.height or pt.x<0 or pt.y<0 or self.grid[pt.x][pt.y]==1:
                 return True
         return False
 
     def move(self, cmd):
         self.current_unit.move(cmd)
         if self.is_incorrect():
-            # do stuff
+            self.current_unit.move_opposite(cmd)
+            for pt in self.current_unit.pts:
+                self.grid[pt.x][pt.y] = 1
+            self.next_unit()
 
+
+    def render_grid(self):
+        assert len(self.grid) > 0
+        assert len(self.grid[0]) > 0
+
+        height = len(self.grid)
+        width = len(self.grid[0])
+        for row in self.grid:
+            assert len(row) == width
+
+        output = RENDER_TOP_LEFT + RENDER_TOP_MID*(width-1) + RENDER_TOP_RIGHT
+
+        row_index = 0
+        while row_index < height:
+
+            if row_index % 2:
+                output += RENDER_BODY_OFFSET
+            for i in range(width):
+                output += RENDER_BODY_LEFTMID.format(RENDER_GRID_FILL if self.grid[row_index][i] or self.current_unit.is_filled(row_index,i) else RENDER_GRID_EMPTY)
+            output += RENDER_BODY_RIGHT
+
+            if row_index != height-1:
+                if row_index % 2:
+                    output += RENDER_EVENJOIN_LEFT + RENDER_EVENJOIN_MID*(width-1) + RENDER_EVENJOIN_RIGHT
+                else:
+                    output += RENDER_ODDJOIN_LEFT + RENDER_ODDJOIN_MID*(width-1) + RENDER_ODDJOIN_RIGHT
+
+            row_index += 1
+
+        if (height-1) % 2:
+            output += RENDER_BODY_OFFSET
+        output += RENDER_BOTTOM_LEFT + RENDER_BOTTOM_MID*(width-1) + RENDER_BOTTOM_RIGHT
+
+        return output
 
 
--- unit.py
+++ unit.py
@@ -18,16 +18,55 @@ class Unit:
     def move_w(self):
         self.pre_move()
         for pt in self.pts:
-            pt += Pt.E
+            pt += W
 
     def move_e(self):
         self.pre_move()
         for pt in self.pts:
-            pt += Pt.W
+            pt += E
+    def move_sw(self):
+        self.pre_move()
+        for pt in self.pts:
+            pt += SW
+
+    def move_se(self):
+        self.pre_move()
+        for pt in self.pts:
+            pt += SE
+    def move_nw(self):
+        self.pre_move()
+        for pt in self.pts:
+            pt += NW
+
+    def move_ne(self):
+        self.pre_move()
+        for pt in self.pts:
+            pt += NE
 
     def move(self,cmd):
-        if cmd==E:
+        if cmd=='E':
             self.move_e()
-        elif cmd==W:
+        elif cmd=='W':
             self.move_w()
+        if cmd=='SE':
+            self.move_se()
+        elif cmd=='SW':
+            self.move_sw()
         self.moves.append(cmd)
+
+    def move_opposite(self,cmd):
+        if cmd=='E':
+            self.move_w()
+        elif cmd=='W':
+            self.move_e()
+        if cmd=='SE':
+            self.move_nw()
+        elif cmd=='SW':
+            self.move_ne()
+        self.moves.append(cmd)
+
+    def is_filled(self,x,y):
+        for pt in self.pts:
+            if pt.x==x and pt.y ==y:
+                return True
+        return False
