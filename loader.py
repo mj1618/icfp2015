47709743f1db4aebe36e0a9401cbe761fdb8769e
sqweek <sqweek@gmail.com>
2015-08-10 03:29:12 +0800

    Submit solutions using default ssl context.
    (doesn't require any cacert filename)

--- loader.py
+++ loader.py
@@ -32,10 +32,10 @@ def get_qualifier_problems(*args):
     return problem_data
 
 def submit(data):
-    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-    context.verify_mode = ssl.CERT_REQUIRED
-    context.load_verify_locations('C:\\Users\\Matt\\cacert.pem')
-    h = http.client.HTTPSConnection('davar.icfpcontest.org',443,context=context)
+    #context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+    #context.verify_mode = ssl.CERT_REQUIRED
+    #context.load_verify_locations('C:\\Users\\Matt\\cacert.pem')
+    h = http.client.HTTPSConnection('davar.icfpcontest.org',443,context=ssl.create_default_context())
     # url_params = urllib.parse.urlencode(json.dumps(data))
     print("Submitting: "+json.dumps(data))
     headers = { 'Content-Type' : 'application/json', "Authorization" : 'Basic '+base64.b64encode(b":lI/jYDtQwdrf4s+SDq6WW91LW5bXpH04ZWhIUI+clxo=").decode("ascii") }
